5e7857324976f640b547df532a897ce0c361767b
Natan Macedo <natancmacedo@gmail.com>
2017-12-20 17:43:49 -0200

    Retirando importações desnecessárias

--- speedDiagnostic/src/speeddiagnostic/SpeedDiagnostic.java
+++ speedDiagnostic/src/speeddiagnostic/SpeedDiagnostic.java
@@ -5,16 +5,7 @@
  */
 package speeddiagnostic;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
 
 /**
  *
